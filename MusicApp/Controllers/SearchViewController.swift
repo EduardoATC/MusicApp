cc6122a93dc5bd335c5de5a1a38c88e6aaf6f679
Eduardo Terrero Cabrera <eduardo.terrero.c@hotmail.com>
2020-07-28 17:40:30 +0200

    Start point

---
+++ MusicApp/Controllers/SearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchViewController.swift
+//  MusicApp
+//
+//  Created by Eduardo Antonio Terrero Cabrera on 28/07/2020.
+//  Copyright © 2020 Eduardo Antonio Terrero Cabrera. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
---
+++ MusicApp/Controllers/TabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  TabBarController.swift
+//  MusicApp
+//
+//  Created by Eduardo Antonio Terrero Cabrera on 28/07/2020.
+//  Copyright © 2020 Eduardo Antonio Terrero Cabrera. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       // view.backgroundColor
+        view.backgroundColor = .systemBackground
+        view.tintColor = .systemPink
+    }
+    
+
+    
+
+}
